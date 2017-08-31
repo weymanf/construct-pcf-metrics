8a9b12e5df673b2d5f798cd527e53d71cdc34f05
Todd Persen <tpersen@pivotal.io>
2017-08-31 14:54:44 -0700

    Start tweaking terminal preferences

--- preferences/install.sh
+++ preferences/install.sh
@@ -3,3 +3,11 @@
 dir=$(cd $(dirname $0); pwd)
 
 cp -f "$dir/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+
+# iTerm2
+/usr/libexec/PlistBuddy -c "Set :\"New Bookmarks\":0:\"Normal Font\" \"InconsolataForPowerline 14\""  ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Merge '$dir/Solarized Dark.itermcolors' :'Custom Color Presets':'Solarized Dark'" ~/Library/Preferences/com.googlecode.iterm2.plist
+
+open /Applications/Docker.app
+open /Applications/Screenhero.app
+open /Applications/ShiftIt.app

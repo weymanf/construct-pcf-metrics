f6da329ecb9cf9fd45275d653df811fdfa250e9e
Todd Persen <tpersen@pivotal.io>
2017-08-31 15:59:24 -0700

    Add Karabiner Elements and ide prefs via post-install

--- homebrew-bundle/install.sh
+++ homebrew-bundle/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 brew tap homebrew/bundle
+
+cd $(dirname $0)
+
 brew bundle
--- preferences/install.sh
+++ preferences/install.sh
@@ -3,6 +3,8 @@
 dir=$(cd $(dirname $0); pwd)
 
 cp -f "$dir/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+mkdir -p "$HOME/.config/karabiner"
+cp -f "$dir/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
 
 # iTerm2
 /usr/libexec/PlistBuddy -c "Set :\"New Bookmarks\":0:\"Normal Font\" \"InconsolataForPowerline 14\""  ~/Library/Preferences/com.googlecode.iterm2.plist
